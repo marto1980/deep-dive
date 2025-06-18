403994d559bc4102f0c13d0c7a69763f5aef58df
Martin Aleksandrov Ivanov <marto1980@gmail.com>
2025-06-18 10:05:36 +0300

    Extract a Separate Header Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core'
 
+import { Header } from './header/header'
+
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [Header],
 })
 export class AppComponent {
   dummyTrafficData = [
