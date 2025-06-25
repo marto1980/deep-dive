89d25f69e83937f86608545ccfdc7692075e0ab1
Martin Aleksandrov Ivanov <marto1980@gmail.com>
2025-06-25 10:16:58 +0300

    A Not-Ideal Way to Extend Built-in Elements

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core'
 
+import { Button } from '../shared/button/button'
+
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [Button],
   templateUrl: './header.html',
 })
 export class Header {}
