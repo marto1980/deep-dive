36d085550547a166d80142c465fa040a1fbc57b3
Martin Aleksandrov Ivanov <marto1980@gmail.com>
2025-07-09 10:59:56 +0300

    Manipulate State & Using Literal Values

--- src/app/dashboard/server-status/server-status.ts
+++ src/app/dashboard/server-status/server-status.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core'
+import { isPlatformBrowser } from '@angular/common'
+import {
+  Component,
+  inject,
+  OnDestroy,
+  OnInit,
+  PLATFORM_ID,
+} from '@angular/core'
 
 @Component({
   selector: 'app-server-status',
   templateUrl: './server-status.html',
   styleUrl: './server-status.scss',
 })
-export class ServerStatus {
-  currentStatus = 'online'
+export class ServerStatus implements OnInit, OnDestroy {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'offline'
+  private intervalId?: ReturnType<typeof setInterval>
+  private readonly platformId = inject(PLATFORM_ID)
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.intervalId = setInterval(() => {
+        // eslint-disable-next-line sonarjs/pseudo-random
+        const rnd = Math.random()
+        if (rnd < 0.5) {
+          this.currentStatus = 'online'
+        } else if (rnd < 0.9) {
+          this.currentStatus = 'offline'
+        } else {
+          this.currentStatus = 'unknown'
+        }
+      }, 5000)
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId)
+    }
+  }
 }
