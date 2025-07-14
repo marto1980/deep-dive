ca466f804b65e77e5105fef35092189c5b747d9f
Martin Aleksandrov Ivanov <marto1980@gmail.com>
2025-07-14 10:27:40 +0300

    Component Cleanup with DestroyRef

--- src/app/dashboard/server-status/server-status.ts
+++ src/app/dashboard/server-status/server-status.ts
@@ -2,8 +2,8 @@ import { isPlatformBrowser } from '@angular/common'
 import {
   AfterViewInit,
   Component,
+  DestroyRef,
   inject,
-  OnDestroy,
   OnInit,
   PLATFORM_ID,
 } from '@angular/core'
@@ -13,15 +13,15 @@ import {
   templateUrl: './server-status.html',
   styleUrl: './server-status.scss',
 })
-export class ServerStatus implements OnInit, OnDestroy, AfterViewInit {
+export class ServerStatus implements OnInit, AfterViewInit {
   currentStatus: 'online' | 'offline' | 'unknown' = 'offline'
-  private interval?: NodeJS.Timeout
   private readonly platformId = inject(PLATFORM_ID)
+  private readonly destroyRef = inject(DestroyRef)
 
   ngOnInit() {
     console.log('ON INIT')
     if (isPlatformBrowser(this.platformId)) {
-      this.interval = setInterval(() => {
+      const interval = setInterval(() => {
         // eslint-disable-next-line sonarjs/pseudo-random
         const rnd = Math.random()
         if (rnd < 0.5) {
@@ -32,16 +32,14 @@ export class ServerStatus implements OnInit, OnDestroy, AfterViewInit {
           this.currentStatus = 'unknown'
         }
       }, 5000)
+      this.destroyRef.onDestroy(() => {
+        console.log('DESTROY')
+        clearInterval(interval)
+      })
     }
   }
 
   ngAfterViewInit() {
     console.log('AFTER VIEW INIT')
-  }
-
-  ngOnDestroy() {
-    if (this.interval) {
-      clearInterval(this.interval)
-    }
   }
 }
