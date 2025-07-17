8de449d201d51cd8de56dc46ee8c718a23ef33a3
Martin Aleksandrov Ivanov <marto1980@gmail.com>
2025-07-17 10:12:19 +0300

    Handle Form Submissions: Repetition

--- src/app/dashboard/tickets/new-ticket/new-ticket.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core'
+import { FormsModule } from '@angular/forms'
 
 import { Button } from '../../../shared/button/button'
 import { Control } from '../../../shared/control/control'
 
 @Component({
   selector: 'app-new-ticket',
-  imports: [Button, Control],
+  imports: [Button, Control, FormsModule],
   templateUrl: './new-ticket.html',
   styleUrl: './new-ticket.scss',
 })
-export class NewTicket {}
+export class NewTicket {
+  onSubmit() {
+    console.log('FORM SUBMITTED!')
+  }
+}
