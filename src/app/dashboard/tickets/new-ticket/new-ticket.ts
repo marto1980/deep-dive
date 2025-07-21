d88c40bdc3d5b780fcd121a47f43ca83b62875b9
Martin Aleksandrov Ivanov <marto1980@gmail.com>
2025-07-21 09:33:41 +0300

    ViewChild vs ContentChild

--- src/app/dashboard/tickets/new-ticket/new-ticket.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.ts
@@ -1,4 +1,10 @@
-import { Component, ElementRef, viewChild } from '@angular/core'
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core'
 import { FormsModule } from '@angular/forms'
 
 import { Button } from '../../../shared/button/button'
@@ -10,11 +16,22 @@ import { Control } from '../../../shared/control/control'
   templateUrl: './new-ticket.html',
   styleUrl: './new-ticket.scss',
 })
-export class NewTicket {
-  private readonly form =
-    viewChild.required<ElementRef<HTMLFormElement>>('form')
+export class NewTicket implements OnInit, AfterViewInit {
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>
+  // private readonly form =
+  //   viewChild.required<ElementRef<HTMLFormElement>>('form')
+  ngOnInit() {
+    console.log('ON INIT')
+    console.log(this.form?.nativeElement)
+  }
+
+  ngAfterViewInit() {
+    console.log('AFTER VIEW INIT')
+    console.log(this.form?.nativeElement)
+  }
+
   onSubmit(title: string, ticketText: string) {
-    this.form().nativeElement.reset()
+    this.form?.nativeElement.reset()
     console.log('title', title)
     console.log('ticketText', ticketText)
   }
